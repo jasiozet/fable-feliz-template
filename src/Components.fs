1a32e4da57b7142fd2fb256274368ed3580bf9f9
Jan Zalewski <18668118+jasiozet@users.noreply.github.com>
2022-07-16 12:14:45 +0200

    cleanup

--- src/Components.fs
+++ src/Components.fs
@@ -4,16 +4,17 @@ open Feliz
 open Feliz.DaisyUI
 
 type Components =
-    /// <summary>
-    /// Simple react component with button increase
-    /// </summary>
-    [<ReactComponent>]
-    static member HelloWorld() =
-        let (count, setCount) = React.useState(0)
-        Html.div [
-            Daisy.button.button [
-                button.primary
-                prop.onClick (fun _ -> setCount(count + 1))
-                prop.text ("Increment: " + string count)
-            ]
-        ]
+  /// <summary>
+  /// Simple react component with button increase
+  /// </summary>
+  [<ReactComponent>]
+  static member HelloWorld() =
+    let (count, setCount) = React.useState (0)
+
+    Html.div [
+      Daisy.button.button [
+        button.primary
+        prop.onClick (fun _ -> setCount (count + 1))
+        prop.text ("Increment: " + string count)
+      ]
+    ]
